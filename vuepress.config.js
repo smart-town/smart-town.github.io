7466be65ae2183b0d58389dbd15b16d7bad97933
smalltown <luhh14@foxmail.com>
2021-11-30 11:28:38 +0800

    :rocket: clear initial content and use vuepress-vite for record

---
+++ vuepress.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    lang: "zh-Hans",
+    title: "小城",
+    description: "notes",
+    public: "public",
+    head: [
+        ['link', {rel: 'icon', href: '/images/favicon.ico'}],
+    ],
+    themeConfig: {
+        // logo: '/images/favicon.ico',
+    }
+}
