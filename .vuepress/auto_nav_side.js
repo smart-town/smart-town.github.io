46b1498cc9320c3ba35556ebc13195bd642c0b75
smalltown <luhh14@foxmail.com>
2021-12-02 08:24:27 +0800

    :sparkles: add a script for auto generating nav and sidebar.
    1. very simple script

---
+++ .vuepress/auto_nav_side.js
@@ -0,0 +1,97 @@
+const path = require("path")
+const fs = require("fs")
+const IGNORE_DIR_DEFAULT = [".github", ".vuepress", "dist", "node_modules", "public", ".git"]
+
+const getSubDir = (dir) => {
+    let dirList = fs.readdirSync(dir)
+    return dirList.filter(v => fs.lstatSync(path.resolve(dir, v)).isDirectory())
+}
+const getSubItems = (dir) => {
+    let dirList = fs.readdirSync(dir)
+    return dirList
+}
+const sortDirs = (dirArr) => {
+    console.log("prerare sort:", dirArr)
+    dirArr.sort()
+    let firstIndex = dirArr.indexOf("README.md")
+    
+    if (firstIndex != -1) {
+        let indexEle = dirArr.splice(firstIndex, firstIndex+1)
+        dirArr.unshift(indexEle[0])
+    }
+    console.log("sort:", dirArr)
+    return dirArr
+}
+
+class AutoNavSide {
+    constructor(root, ignoreDirList=IGNORE_DIR_DEFAULT, ignoreFileList) {
+        this.root = root ? root : process.cwd()
+        this.ignoreDirList = ignoreDirList
+        this.structure = this.getStructure()
+
+        console.log(`
+----------------------------------------------------------
+root: ${this.root}
+ignore: ${this.ignoreDirList}
+structure: \n${JSON.stringify(this.structure, null, 4)}
+----------------------------------------------------------
+        `)
+    }
+    getStructure() {
+        this.autoGroupDir = fs.readdirSync(this.root).filter(dir => this.ignoreDirList.indexOf(dir) == -1 )
+        let structure = {}
+        for (let baseDir of this.autoGroupDir) {
+            let temp = path.resolve(this.root, baseDir)
+            if (!fs.lstatSync(temp).isDirectory()) {
+                continue
+            }
+            structure[baseDir] = getSubDir(temp)
+        }
+        return structure
+    }
+    
+    genNavbarGroup() {
+        let result = []
+    
+        for (let dir of Object.keys(this.structure)) {
+            let subDir = this.structure[dir]
+            if (subDir.length > 0) {
+                result.push({
+                    text: dir,
+                    children: subDir.map(sub => `/${dir}/${sub}/`) 
+                })
+            } else {
+                result.push({
+                    text: dir,
+                    link: `/${dir}/README.md`
+                })
+            }
+        }
+        return result
+    }
+    genSidebarGroup() {
+        // subArrKey = Object.keys(this.subDict)
+        // let result = {}
+        let result = {}
+        for (let dir of Object.keys(this.structure)) {
+            let subDir = this.structure[dir]
+            if (subDir.length > 0) {
+                for (let sub of subDir) {
+                    let temp = path.resolve(this.root, dir, sub)
+                    result[`/${dir}/${sub}/`] = sortDirs(getSubItems(temp)).map(v => `/${dir}/${sub}/${v}`)
+                }
+            } else {
+                let temp = path.resolve(this.root, dir)
+                result[`/${dir}/`] = sortDirs(getSubItems(temp)).map(v => `/${dir}/${v}`)
+            }
+        }
+        return result
+    }
+}
+
+let test = new AutoNavSide()
+console.log(test.genNavbarGroup())
+console.log(test.genSidebarGroup())
+module.exports = {
+    AutoNavSide,
+}
--- vuepress.config.js
+++ vuepress.config.js
@@ -1,3 +1,10 @@
+const {AutoNavSide} = require("./.vuepress/auto_nav_side")
+
+const autoNavSide = new AutoNavSide()
+let navbar = autoNavSide.genNavbarGroup()
+let sidebar = autoNavSide.genSidebarGroup()
+console.log(navbar)
+console.log(sidebar)
 module.exports = {
     lang: "zh-Hans",
     title: "小城",
@@ -9,5 +16,7 @@ module.exports = {
     ],
     themeConfig: {
         // logo: '/images/favicon.ico',
+        navbar,
+        sidebar,
     }
 }
